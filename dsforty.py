672fdf73d01a5007ff9851344d303fe49465105e
Mansour Behabadi <mansour@oxplot.com>
2015-11-26 09:41:14 +1100

    Autocrop the width as well
    - also got rid of mono color (just process the grayscale if needed)
    - refactored some code
    - the thresholds for autocrop is currently hard coded, would probably
      want to be able to pass args to control it

--- dsforty.py
+++ dsforty.py
@@ -18,7 +18,24 @@ EP_IN = 0x81
 MAX_W = 8.5 # in inches
 MAX_H = 14 # in inches
 
-CLRS = {'m': b'M001', 'g': b'M008', 'c': b'C024'}
+CLRS = {'g': b'M008', 'c': b'C024'}
+BLANKS = {'g': 45, 'c': (44, 45, 45)}
+BLANK_VARI = 25
+MIN_HFEATURE_LEN = 0.3937 # in inches ~ 1mm
+
+def find_edge(line, blank, vari, streak_thresh):
+  streak = 0
+  low = [max(0, c - vari) for c in blank]
+  high = [min(255, c + vari) for c in blank]
+  for pi, p in enumerate(line):
+    for c, cl, ch in zip(p, low, high):
+      if c < cl or c > ch:
+        streak += 1
+        if streak > streak_thresh:
+          return pi - streak + 1
+        break
+      streak = 0
+  return 0
 
 def main():
   parser = argparse.ArgumentParser(
@@ -26,7 +43,7 @@ def main():
     description='Scan a page on Epson DS-40'
   )
   parser.add_argument(
-    '-r', '--resolution',
+    '-r', '--res',
     choices=[300, 600],
     default=300,
     type=int,
@@ -36,7 +53,7 @@ def main():
     '-c', '--color',
     choices=list(sorted(CLRS)),
     default='c',
-    help='Scan color: `m` for mono, `g` for grayscale and `c` for color'
+    help='Scan color: `g` for grayscale and `c` for color'
          ' - defaults to color'
   )
   args = parser.parse_args()
@@ -73,14 +90,14 @@ def main():
   # CMX sets color correction: (UNIT -> disable, UM08 -> for C024)
   # RSM, RSS sets resolution
 
-  width, height = args.resolution * MAX_W, args.resolution * MAX_H
+  width, height = int(args.res * MAX_W), int(args.res * MAX_H)
 
   params = deque()
   params.append(b'#ADF#COL')
   params.append(CLRS[args.color])
   params.append(b'#FMTJPG #JPGd100#GMMUG18#CMXUNIT')
   params.append(
-    b'#RSMi%07d#RSSi%07d' % (args.resolution, args.resolution))
+    b'#RSMi%07d#RSSi%07d' % (args.res, args.res))
   params.append(b'#ACQi%07di%07di%07di%07d' % (0, 0, width, height))
   params.append(b'#PAGd000#BSZi%07d' % IN_BUF_SIZE)
   for gmt in (b'RED', b'GRN', b'BLU'):
@@ -129,11 +146,35 @@ def main():
     print('no final height reported', file=sys.stderr)
     exit(1)
 
-  # Crop the height
+  # Analyse the output for better cropping if PIL is available
+
+  left, right = 0, width
+  try:
+    from PIL import Image
+
+    vnorm = (lambda x: (x, x, x)) if args.color == 'g' else lambda x: x
+    line = [vnorm(p)
+      for p in Image.open(tmpout).crop((0, 0, width, final_height))
+        .resize((width, 1), 1).getdata()]
+    streak_th = int((MIN_HFEATURE_LEN / MAX_H) * len(line))
+    blank = vnorm(BLANKS[args.color])
+    left = find_edge(line, blank, BLANK_VARI, streak_th)
+    right = find_edge(reversed(line), blank, BLANK_VARI, streak_th)
+    right = width - right
+    if left >= right:
+      left, right = 0, width
+      print('couldn\'t find edges', file=sys.stderr)
+    tmpout.seek(0)
+
+  except ImportError:
+    pass
+
+  # Crop
 
   jpegtran = Popen(
-    ['jpegtran', '-crop', '%dx%d+0+0' % (width, final_height)],
-    stdin=tmpout, stderr=open(os.devnull, 'wb')
+    ['jpegtran', '-crop', '%dx%d+%d+0' % (
+      right - left, final_height, left
+    )], stdin=tmpout, stderr=open(os.devnull, 'wb')
   )
   jpegtran.wait()
   exit(0)
